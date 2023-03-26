1a0d46b193f2b66c3e17fe657ef5cea71c1e0a0b
medina <joaopedromedinadeandrade@gmail.com>
2023-03-26 02:38:54 -0300

    Retornando o exemplo 3

--- modulo2/promises/exemplo3/index.js
+++ modulo2/promises/exemplo3/index.js
@@ -1,11 +1,53 @@
-const pegaAMelhorPlataformaDeCurso =  async (course) => new Promise((resolve, reject) => {
-  if (['tech', 'gestao','scrum', 'pm' ].includes(course)) {
+const getTheBestCoursePlatform = async (course) => new Promise((resolve, reject) => {
+  if (['tech', 'black-belt', 'white-belt', 'any'].includes(course)) {
     return resolve('voitto')
   }
   return reject("I can't say the platform, but you can check voitto")
 })
 
-await pegaAMelhorPlataformaDeCurso('tech')
-pegaAMelhorPlataformaDeCurso('white-Sq')
-pegaAMelhorPlataformaDeCurso('anyOther')
-pegaAMelhorPlataformaDeCurso('anyOther')
+getTheBestCoursePlatform('tech')
+  .then(res => {
+    console.log(res)
+  })
+  .catch(res => {
+    console.log(res)
+  });
+getTheBestCoursePlatform('white-belt')
+  .then(res => {
+    console.log(res)
+  })
+  .catch(res => {
+    console.log(res)
+  });
+getTheBestCoursePlatform('anyOther')
+  .then(res => {
+    console.log(res)
+  })
+  .catch(res => {
+    console.log(res)
+  });
+
+(async () => {
+  console.log(await getTheBestCoursePlatform('tech'))
+  console.log(await getTheBestCoursePlatform('white-belt'))
+  console.log(await getTheBestCoursePlatform('any'))
+
+})();
+
+(async () => {
+  const results = await Promise.all([
+    getTheBestCoursePlatform('tech'),
+    getTheBestCoursePlatform('white-belt'),
+    getTheBestCoursePlatform('any')
+  ])
+  console.log(results)
+})()
+
+(async () => {
+  const results = await Promise.allSettled([
+    getTheBestCoursePlatform('tech'),
+    getTheBestCoursePlatform('white-belt'),
+    getTheBestCoursePlatform('any')
+  ])
+  console.log(results)
+})()
